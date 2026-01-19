f2b28fd1fef90fef97f596eba27acf4627603929
Jstepniewsky <kubbonekk@gmail.com>
2026-01-19 01:41:37 +0100

    admin.py zmieniony

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models_fivem import Users
 
-# Register your models here.
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'firstname', 'lastname', 'job', 'job_grade', 'phone_number')
+    search_fields = ('identifier', 'firstname', 'lastname', 'job')
