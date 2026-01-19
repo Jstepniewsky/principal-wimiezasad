ec12cbab54aa7a516bb7a12a6189753065e8e4f2
Jstepniewsky <kubbonekk@gmail.com>
2026-01-19 01:30:42 +0100

    naprawa css po stronie serwera

--- core/settings.py
+++ core/settings.py
@@ -121,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'                # już masz
+STATIC_ROOT = BASE_DIR / 'staticfiles'
