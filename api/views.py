0c883bf8232f12793c58c5aa1bee546f934c1997
Kuba Stępniewski <kubas@mbp-kuba.home>
2026-01-29 19:14:23 +0100

    przegladarka utworzonych modeli - fix 2

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models_fivem import Users
 from .serializers import UsersSerializer
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
