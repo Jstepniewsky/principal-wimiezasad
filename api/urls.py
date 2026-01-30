ef7fc2da1410ddad3d7a326d82230a42ae715ceb
Kuba Stępniewski <kubas@MacBook-Pro-Kuba.local>
2026-01-30 18:22:24 +0100

    bramkarz idenifiera cfx fix 1

--- api/urls.py
+++ api/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('vehicles/<str:pk>/', views.VehiclesDetailView.as_view(), name='vehicles-detail'),
     path('stats/', views.ServerStatsView.as_view(), name='server-stats'),
     path('check-license/<int:player_id>/', views.CheckLicenseView.as_view(), name='check-license'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
